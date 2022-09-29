87e5b219e0db10ce85af088fa334e3ad2debba1e
Jeff Lett <jeffctown@gmail.com>
2022-09-29 06:57:40 -0400

    Update character-classes.js

--- data/character-classes.js
+++ data/character-classes.js
@@ -4,11 +4,41 @@
     "key": "gloomhaven",
     "classes": [
       {
+        "name": "Beast Tyrant",
+        "images": {
+          "mat": {
+            "front": "character-mats/gloomhaven/gh-beast-tyrant.png",
+            "back": "character-mats/gloomhaven/gh-beast-tyrant-back.png"
+          }
+        },
+        "unlockable": true
+      },
+      {
+        "name": "Berserker",
+        "images": {
+          "mat": {
+            "front": "character-mats/gloomhaven/gh-berserker.png",
+            "back": "character-mats/gloomhaven/gh-berserker-back.png"
+          }
+        },
+        "unlockable": true
+      },
+      {
+        "name": "Bladeswarm",
+        "images": {
+          "mat": {
+            "front": "character-mats/gloomhaven/gh-bladeswarm.png",
+            "back": "character-mats/gloomhaven/gh-bladeswarm-back.png"
+          }
+        },
+        "unlockable": true
+      },
+      {
         "name": "Brute",
         "images": {
           "mat": {
             "front": "character-mats/gloomhaven/gh-brute.png",
-            "back":  "character-mats/gloomhaven/gh-brute-back.png"
+            "back": "character-mats/gloomhaven/gh-brute-back.png"
           }
         }
       },
@@ -22,13 +52,74 @@
         }
       },
       {
+        "name": "Doomstalker",
+        "images": {
+          "mat": {
+            "front": "character-mats/gloomhaven/gh-doomstalker.png",
+            "back": "character-mats/gloomhaven/gh-doomstalker-back.png"
+          }
+        },
+        "unlockable": true
+      },
+      {
+        "name": "Elementalist",
+        "images": {
+          "mat": {
+            "front": "character-mats/gloomhaven/gh-elementalist.png",
+            "back": "character-mats/gloomhaven/gh-elementalist-back.png"
+          }
+        },
+        "unlockable": true
+      },
+      {
         "name": "Mindthief",
         "images": {
           "mat": {
             "front": "character-mats/gloomhaven/gh-mindthief.png",
             "back": "character-mats/gloomhaven/gh-mindthief-back.png",
+
           }
         }
+      },
+      {
+        "name": "Nightshroud",
+        "images": {
+          "mat": {
+            "front": "character-mats/gloomhaven/gh-nightshroud.png",
+            "back": "character-mats/gloomhaven/gh-nightshroud-back.png"
+          }
+        },
+        "unlockable": true
+      },
+      {
+        "name": "Plagueherald",
+        "images": {
+          "mat": {
+            "front": "character-mats/gloomhaven/gh-plagueherald.png",
+            "back": "character-mats/gloomhaven/gh-plagueherald-back.png"
+          }
+        },
+        "unlockable": true
+      },
+      {
+        "name": "Quartermaster",
+        "images": {
+          "mat": {
+            "front": "character-mats/gloomhaven/gh-quartermaster.png",
+            "back": "character-mats/gloomhaven/gh-quartermaster-back.png"
+          }
+        },
+        "unlockable": true
+      },
+      {
+        "name": "Sawbones",
+        "images": {
+          "mat": {
+            "front": "character-mats/gloomhaven/gh-sawbones.png",
+            "back": "character-mats/gloomhaven/gh-sawbones-back.png"
+          }
+        },
+        "unlockable": true
       },
       {
         "name": "Scoundrel",
@@ -40,6 +131,16 @@
         }
       },
       {
+        "name": "Soothsinger",
+        "images": {
+          "mat": {
+            "front": "character-mats/gloomhaven/gh-soothsinger.png",
+            "back": "character-mats/gloomhaven/gh-soothsinger-back.png"
+          }
+        },
+        "unlockable": true
+      },
+      {
         "name": "Spellweaver",
         "images": {
           "mat": {
@@ -49,11 +150,91 @@
         }
       },
       {
+        "name": "Summoner",
+        "images": {
+          "mat": {
+            "front": "character-mats/gloomhaven/gh-summoner.png",
+            "back": "character-mats/gloomhaven/gh-summoner-back.png"
+          }
+        },
+        "unlockable": true
+      },
+      {
+        "name": "Sunkeeper",
+        "images": {
+          "mat": {
+            "front": "character-mats/gloomhaven/gh-sunkeeper.png",
+            "back": "character-mats/gloomhaven/gh-sunkeeper-back.png"
+          }
+        },
+        "unlockable": true
+      },
+      {
         "name": "Tinkerer",
         "images": {
           "mat": {
-              "front": "character-mats/gloomhaven/gh-tinkerer.png",
-              "back": "character-mats/gloomhaven/gh-tinkerer-back.png"
+            "front": "character-mats/gloomhaven/gh-tinkerer.png",
+            "back": "character-mats/gloomhaven/gh-tinkerer-back.png"
+          }
+        }
+      }
+    ]
+  },
+  {
+    "name": "Frosthaven",
+    "key": "frosthaven",
+    "classes": [
+      {
+        "name": "Banner Spear",
+        "images": {
+          "mat": {
+            "front": "character-mats/frosthaven/fh-banner-spear.png",
+            "back": "character-mats/frosthaven/fh-banner-spear-back.png"
+          }
+        }
+      }
+    ]
+  },
+  {
+    "name": "Forgotten Circles",
+    "key": "forgottencircles",
+    "classes": [
+      {
+        "name": "Diviner",
+        "images": {
+          "mat": {
+            "front": "character-mats/forgotten-circles/fc-diviner.png",
+            "back": "character-mats/forgotten-circles/fc-diviner-back.png"
+          }
+        }
+      }
+    ]
+  },
+  {
+    "name": "Jaws of the Lion",
+    "key": "jawsofthelion",
+    "classes": [
+      {
+        "name": "Demolitionist",
+        "images": {
+          "mat": {
+            "front": "character-mats/jaws-of-the-lion/jl-demolitionist-front.png",
+            "back": "character-mats/jaws-of-the-lion/jl-demolitionist-back.png"
+          }
+        }
+      }
+    ]
+  },
+  {
+    "name": "Crimson Scales",
+    "key": "crimsonscales",
+    "classes": [
+      {
+        "name": "Amber Aegis",
+        "images": {
+          "mat": {
+            "front": "character-mats/crimson-scales/cs-amber-aegis.png",
+            "back": "character-mats/crimson-scales/cs-amber-aegis-back.png"
           }
         }
       }
