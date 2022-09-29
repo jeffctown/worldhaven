b40822eabeef7280d3d5698145a97aed9a1cf164
Jeff Lett <jeffctown@gmail.com>
2022-09-28 22:45:13 -0400

    Create character-classes.js

---
+++ data/character-classes.js
@@ -0,0 +1,67 @@
+[
+  {
+    "name": "Gloomhaven",
+    "classes": [
+      {
+        "name": "Brute",
+        "expansion": "Gloomhaven",
+        "images": {
+          "mat": {
+            "front": "character-mats/gloomhaven/gh-brute.png",
+            "back":  "character-mats/gloomhaven/gh-brute-back.png"
+          }
+        }
+      },
+      {
+        "name": "Cragheart",
+        "expansion": "Gloomhaven",
+        "images": {
+          "mat": {
+            "front": "character-mats/gloomhaven/gh-cragheart.png",
+            "back": "character-mats/gloomhaven/gh-cragheart-back.png"
+          }
+        }
+      },
+      {
+        "name": "Mindthief",
+        "expansion": "Gloomhaven",
+        "images": {
+          "mat": {
+            "front": "character-mats/gloomhaven/gh-mindthief.png",
+            "back": "character-mats/gloomhaven/gh-mindthief-back.png",
+          }
+        }
+      },
+      {
+        "name": "Scoundrel",
+        "expansion": "Gloomhaven",
+        "images": {
+          "mat": {
+            "front": "character-mats/gloomhaven/gh-scoundrel.png",
+            "back": "character-mats/gloomhaven/gh-scoundrel-back.png"
+          }
+        }
+      },
+      {
+        "name": "Spellweaver",
+        "expansion": "Gloomhaven",
+        "images": {
+          "mat": {
+            "front": "character-mats/gloomhaven/gh-spellweaver.png",
+            "back": "character-mats/gloomhaven/gh-spellweaver-back.png"
+          }
+        }
+      },
+      {
+        "name": "Tinkerer",
+        "expansion": "Gloomhaven",
+        "images": {
+          "mat": {
+              "front": "character-mats/gloomhaven/gh-tinkerer.png",
+              "back": "character-mats/gloomhaven/gh-tinkerer-back.png"
+          }
+        }
+      }
+    ]
+  }
+]
